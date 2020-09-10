8f433680eec1f430d8f3105a4fe806439c13771e
Samuel Silbory <sam@nirmata.com>
2020-09-10 12:48:15 -0700

    commit configmap builder script

---
+++ build-configmap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+mkdir -p ca-certs
+
+# Run docker to build certs.  We are using Ubuntu as it's easier to do.
+docker run --rm -it --mount type=bind,source=$(pwd),target=/certs ubuntu /certs/docker-scripts/build-ca-cert.sh
+
+# build a configmap
+kubectl create configmap site-certs --dry-run --from-file=ca-certs/ -o yaml > site-certs.yaml
+echo created site-certs.yaml configmap
+
+# cleanup
+#rm -f ca-certs/*
+
