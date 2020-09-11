1affaa15071f8e2d50695ac72857a28005f2c9e8
Samuel Silbory <sam@nirmata.com>
2020-09-11 14:54:18 -0700

    add shellcheck recommended quotes, and removed some white space

--- build-configmap.sh
+++ build-configmap.sh
@@ -1,9 +1,9 @@
-#!/bin/bash 
+#!/bin/bash
 
 mkdir -p ca-certs
 
 # Run docker to build certs.  We are using Ubuntu as it's easier to do.
-docker run --rm -it --mount type=bind,source=$(pwd),target=/certs ubuntu /certs/docker-scripts/build-ca-cert.sh
+docker run --rm -it --mount type=bind,source="$(pwd)",target=/certs ubuntu /certs/docker-scripts/build-ca-cert.sh
 
 # build a configmap
 kubectl create configmap site-certs --dry-run --from-file=ca-certs/ -o yaml > site-certs.yaml
@@ -11,4 +11,3 @@ echo created site-certs.yaml configmap
 
 # cleanup
 #rm -f ca-certs/*
-
--- docker-scripts/build-ca-cert.sh
+++ docker-scripts/build-ca-cert.sh
@@ -1,8 +1,8 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 # We need to install ca-certificates which is not in the base docker image for Ubuntu
 echo install ca-certificates
-apt-get update 
+apt-get update
 apt-get -y install ca-certificates openjdk-11-jre-headless
 echo ; echo
 
@@ -13,7 +13,7 @@ rm -f /usr/local/share/ca-certificates/*
 echo build java cacerts keystore
 cp /certs/certs/*.crt /usr/local/share/ca-certificates
 
-if [ -e /certs/certs/cacerts ];then 
+if [ -e /certs/certs/cacerts ];then
     cp /certs/certs/cacerts /certs/ca-certs/cacerts
 else
     cp /certs/certs/cacerts /etc/ssl/certs/java/cacerts
@@ -21,14 +21,13 @@ fi
 
 for cert in /certs/certs/*.crt;do
     # Note I'm assuming the password is the standard one
-    keytool  -import  -trustcacerts  --alias "site$(basename -- $cert)" -file $cert -keystore /certs/ca-certs/cacerts -storepass changeit
+    keytool  -import  -trustcacerts  --alias "site$(basename -- $cert)" -file "$cert" -keystore /certs/ca-certs/cacerts -storepass changeit
 done
 
 # generate your cert bundle
 echo build cert bundle
 ls /usr/local/share/ca-certificates/* >> /etc/ca-certificates.conf
-update-ca-certificates 
+update-ca-certificates
 
 # Store cert bundle in Red Hat and Debian styles
 cp /etc/ssl/certs/ca-certificates.crt /certs/ca-certs/
-
