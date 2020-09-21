8a2b973f5830bcadee1de816939f02859a802bea
silborynirmata <49996154+silborynirmata@users.noreply.github.com>
2020-09-21 09:44:34 -0700

    Cleanup script for reabablity

--- docker-scripts/build-ca-cert.sh
+++ docker-scripts/build-ca-cert.sh
@@ -1,17 +1,11 @@
-#!/bin/bash 
+#!/bin/bash
 
-# We need to install ca-certificates which is not in the base docker image for Ubuntu
-echo install ca-certificates
+# We need to install ca-certificates, and jdk
+echo install ca-certificates and jdk
 apt-get update
 apt-get -y install ca-certificates openjdk-11-jre-headless
-echo ; echo
 
-# Create local cert files and tel ca-certificates to use them
-mkdir -p /usr/local/share/ca-certificates
-rm -f /usr/local/share/ca-certificates/*
-
-echo build java cacerts keystore
-cp /certs/certs/*.crt /usr/local/share/ca-certificates
+echo; echo build java cacerts keystore
 
 if [ -e /certs/certs/cacerts ];then
     cp /certs/certs/cacerts /certs/ca-certs/cacerts
@@ -24,10 +18,15 @@ for cert in /certs/certs/*.crt;do
     keytool  -import  -trustcacerts  --alias "site$(basename -- $cert)" -file "$cert" -keystore /certs/ca-certs/cacerts -storepass changeit
 done
 
+# Position cert files
+mkdir -p /usr/local/share/ca-certificates
+rm -f /usr/local/share/ca-certificates/*
+cp /certs/certs/*.crt /usr/local/share/ca-certificates
+
 # generate your cert bundle
-echo build cert bundle
+echo; echo build cert bundle
 ls /usr/local/share/ca-certificates/* >> /etc/ca-certificates.conf
 update-ca-certificates
 
-# Store cert bundle in Red Hat and Debian styles
+# Store cert bundle
 cp /etc/ssl/certs/ca-certificates.crt /certs/ca-certs/
